1bf8d8618527dac93074c32cd22860cf5b95ca42
Dominik Chrastecky <dominik@chrastecky.cz>
2024-07-07 00:27:14 +0200

    Add CLI tool

---
+++ cli-tools/go.mod
@@ -0,0 +1,11 @@
+module main
+
+go 1.22.2
+
+require (
+	github.com/araddon/dateparse v0.0.0-20210429162001-6b43995a97de // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/mattn/go-runewidth v0.0.10 // indirect
+	github.com/rivo/uniseg v0.1.0 // indirect
+	github.com/scylladb/termtables v0.0.0-20191203121021-c4c0b6d42ff4 // indirect
+)
---
+++ cli-tools/main.go
@@ -0,0 +1,258 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/araddon/dateparse"
+	"github.com/google/uuid"
+	"io"
+	"os"
+	"slices"
+	"strings"
+	"time"
+)
+
+func printHelp(command string, config *map[string]map[string]map[string]string) {
+	topCommands := make([]string, 0, len(*config))
+	for key := range *config {
+		topCommands = append(topCommands, key)
+	}
+
+	if command == "" {
+		fmt.Println("You must use one of the available subcommands:", strings.Join(topCommands, ", "))
+		return
+	}
+
+	if !slices.Contains(topCommands, command) {
+		fmt.Println(command, "is not a valid subcommand, please use one of the following:", strings.Join(topCommands, ", "))
+		return
+	}
+
+	commandConfig := (*config)[command]
+	fmt.Println("Usage:")
+	fmt.Print(os.Args[0] + " " + command)
+
+	flagNames := make([]string, 0, len(commandConfig))
+	for key := range commandConfig {
+		flagNames = append(flagNames, key)
+	}
+
+	slices.SortFunc(flagNames, func(a, b string) int {
+		configA := commandConfig[a]
+		configB := commandConfig[b]
+
+		if configA["required"] == "y" && configB["required"] == "y" {
+			return 0
+		}
+
+		if configA["required"] == "y" {
+			return -1
+		}
+
+		return 1
+	})
+
+	for _, flagName := range flagNames {
+		flagConfig := commandConfig[flagName]
+		fmt.Print(" ")
+		if flagConfig["required"] != "y" {
+			fmt.Print("[")
+		}
+		fmt.Print(" ")
+
+		fmt.Print("--", flagName)
+		fmt.Print(" ")
+
+		fmt.Print("<value>")
+
+		if flagConfig["required"] != "y" {
+			fmt.Print(" ]")
+		}
+	}
+
+	fmt.Println()
+	fmt.Println()
+	fmt.Println("Options:")
+
+	for _, flagName := range flagNames {
+		flagConfig := commandConfig[flagName]
+
+		fmt.Print("  --"+flagName, "\t\t")
+		if flagConfig["required"] == "y" {
+			fmt.Print("(Required)")
+		} else {
+			fmt.Print("(Optional)")
+		}
+		fmt.Print("\t")
+		fmt.Print(flagConfig["description"])
+
+		fmt.Println()
+	}
+}
+
+func validate(config *map[string]map[string]string) bool {
+	for key := range *config {
+		fieldConfig := (*config)[key]
+		if fieldConfig["required"] == "y" && fieldConfig["value"] == "" {
+			return false
+		}
+	}
+
+	return true
+}
+
+func handleAdd(config map[string]map[string]string) int {
+	name := config["title"]["value"]
+	description := config["description"]["value"]
+	link := config["link"]["value"]
+	only := config["only"]["value"]
+	validSince, err := dateparse.ParseAny(config["valid-since"]["value"])
+	if err != nil {
+		fmt.Println("Failed parsing valid-since date:")
+		fmt.Println(err)
+		return 1
+	}
+	validUntil, err := dateparse.ParseAny(config["valid-until"]["value"])
+	if err != nil {
+		fmt.Println("Failed parsing valid-until date:")
+		fmt.Println(err)
+		return 1
+	}
+
+	utc, err := time.LoadLocation("UTC")
+	if err != nil {
+		fmt.Println("Failed loading UTC location")
+		return 1
+	}
+
+	jsonMap := make(map[string]interface{})
+	jsonMap["title"] = name
+	jsonMap["validSince"] = validSince.In(utc).Format(time.RFC3339)
+	jsonMap["validUntil"] = validUntil.In(utc).Format(time.RFC3339)
+	if description != "" {
+		jsonMap["description"] = description
+	}
+	if link != "" {
+		jsonMap["link"] = link
+	}
+	if only != "" {
+		jsonMap["limitedTo"] = strings.Split(only, ",")
+	}
+	jsonMap["id"] = uuid.New().String()
+	if addJson(jsonMap) {
+		fmt.Println("The event has been successfully added")
+		return 0
+	}
+
+	fmt.Println("Failed to create the event")
+	return 0
+}
+
+func addJson(data map[string]interface{}) bool {
+	filename := "../source.json"
+
+	jsonFile, err := os.Open(filename)
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+
+	defer jsonFile.Close()
+
+	bytes, err := io.ReadAll(jsonFile)
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+
+	result := make([]interface{}, 0)
+	err = json.Unmarshal(bytes, &result)
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+
+	result = append(result, data)
+
+	jsonRaw, err := json.MarshalIndent(result, "", "  ")
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+
+	os.WriteFile(filename, jsonRaw, os.ModePerm)
+
+	return true
+}
+
+func main() {
+	commands := make(map[string]map[string]map[string]string)
+	commands["add"] = make(map[string]map[string]string)
+
+	commands["add"]["title"] = make(map[string]string)
+	commands["add"]["title"]["description"] = "The name of the event"
+	commands["add"]["title"]["required"] = "y"
+
+	commands["add"]["description"] = make(map[string]string)
+	commands["add"]["description"]["description"] = "The longer text of the notification, describing the event"
+
+	commands["add"]["link"] = make(map[string]string)
+	commands["add"]["link"]["description"] = "A link that the notification will point to"
+
+	commands["add"]["valid-since"] = make(map[string]string)
+	commands["add"]["valid-since"]["description"] = "The date and time where the event starts, notifications won't be shown beforehand"
+	commands["add"]["valid-since"]["required"] = "y"
+
+	commands["add"]["valid-until"] = make(map[string]string)
+	commands["add"]["valid-until"]["description"] = "The date and time where the event starts, notifications won't be shown beforehand"
+	commands["add"]["valid-until"]["required"] = "y"
+
+	commands["add"]["only"] = make(map[string]string)
+	commands["add"]["only"]["description"] = "Specify the names of the projects this applies to (separated by comma)"
+
+	values := make(map[string]map[string]*string)
+	flagSets := make(map[string]*flag.FlagSet)
+
+	for topCommand := range commands {
+		flagSets[topCommand] = flag.NewFlagSet(topCommand, flag.ExitOnError)
+		values[topCommand] = make(map[string]*string)
+		for flagName := range commands[topCommand] {
+			config := commands[topCommand][flagName]
+			values[topCommand][flagName] = flagSets[topCommand].String(flagName, "", config["description"])
+		}
+	}
+
+	if len(os.Args) < 2 {
+		printHelp("", &commands)
+		os.Exit(1)
+	}
+
+	topCommands := make([]string, 0, len(commands))
+	for key := range commands {
+		topCommands = append(topCommands, key)
+	}
+
+	if !slices.Contains(topCommands, os.Args[1]) {
+		printHelp(os.Args[1], &commands)
+		os.Exit(1)
+	}
+
+	command := os.Args[1]
+	flagSets[command].Parse(os.Args[2:])
+	config := commands[command]
+
+	for flagName := range values[command] {
+		config[flagName]["value"] = *values[command][flagName]
+	}
+
+	if !validate(&config) {
+		printHelp(command, &commands)
+		os.Exit(1)
+	}
+
+	switch command {
+	case "add":
+		os.Exit(handleAdd(config))
+	}
+}
