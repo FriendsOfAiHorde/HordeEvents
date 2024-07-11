ebffdb1e00c6f35bb7270f4d55b209f6918b79d7
Dominik Chrastecky <dominik@chrastecky.cz>
2024-07-11 14:12:13 +0200

    Add support for data to cli tools

--- cli-tools-src/horde/Event.go
+++ cli-tools-src/horde/Event.go
@@ -6,12 +6,13 @@ import (
 )
 
 type Event struct {
-	Id          uuid.UUID `json:"id"`
-	Title       string    `json:"title"`
-	ValidSince  time.Time `json:"validSince"`
-	ValidUntil  time.Time `json:"validUntil"`
-	Description *string   `json:"description,omitempty"`
-	LimitedTo   []string  `json:"limitedTo,omitempty"`
-	Link        *string   `json:"link,omitempty"`
-	Channels    []string  `json:"channels,omitempty"`
+	Id          uuid.UUID                 `json:"id"`
+	Title       string                    `json:"title"`
+	ValidSince  time.Time                 `json:"validSince"`
+	ValidUntil  time.Time                 `json:"validUntil"`
+	Description *string                   `json:"description,omitempty"`
+	LimitedTo   []string                  `json:"limitedTo,omitempty"`
+	Link        *string                   `json:"link,omitempty"`
+	Channels    []string                  `json:"channels,omitempty"`
+	Data        map[string]map[string]any `json:"data,omitempty"`
 }
